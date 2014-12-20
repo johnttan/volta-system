7f77e93bb807b5c58fbfe998d1f3ee3f2363e0f5
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2014-12-19 23:20:10 -0800

    Added gulpfile scripts and test folder

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var stubs = require('./stubs.js');
+
+describe("test for tests", function(){
+
+  it("tests should run", function(){
+    var a = 2;  
+    expect(a).to.equal(2);
+  });
+
+});
