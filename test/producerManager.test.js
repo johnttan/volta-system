341ba39031c6d100329ab0f156a816c0ac669d86
John Tan <tantai.john@gmail.com>
2014-12-24 14:59:44 -0800

    Added testConfig to stubs

--- test/producerManager.test.js
+++ test/producerManager.test.js
@@ -1,16 +1,6 @@
 var expect = require('chai').expect;
 var ProducerManager = require('../producerManager');
-var testConfig = {
-  port: 8000,
-  maxConsumers: 10,
-  maxNumBids: 10,
-  biddingDuration: 3000,
-  blockDuration: 1500,
-  maxPrice: 1,
-  minPrice: 100,
-  margin: 1,
-  defaultPriceAndControl: 'verticalDemandAndStepwiseSupply'
-};
+var testConfig = require('./stubs').config;
 
 describe("producerManager methods", function(){
   var producerManager = new ProducerManager(testConfig);
