8f2d38d570986bcb623d2a14d70992bca845b889
John Tan <tantai.john@gmail.com>
2014-12-28 23:13:44 -0800

    Updated Accounting Server to link to Receipts

--- SystemServer/market/receiptsModel.js
+++ SystemServer/market/receiptsModel.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 var Receipts = function(){
   this.receipts = [];
 };
@@ -7,10 +9,25 @@ Receipts.prototype.addTransaction = function(transaction){
 };
 
 Receipts.prototype.save = function(){
-
+  console.log('sending save')
+  request.post({
+    url: 'http://localhost:8010/api/transactions',
+    json: true,
+    body: this.receipts
+  }, function(err, resp, body){
+    if(err){
+      console.log('err saving receipts', err);
+    }
+  });
 };
 
 Receipts.prototype.getReceipts = function() {
   return this.receipts;
 };
 module.exports = Receipts;
+// var test = new Receipts();
+
+// setInterval(function(){
+//   test.receipts = [{transactionId: Math.random().toString(36), price: 23, consumerId: '2', energy: 10, block:{blockStart: 1, blockDuration: 1}}, {transactionId: 'TEST2', price: 23, consumerId: '2', energy: 10, block:{blockStart: 1, blockDuration: 1}}];
+//   test.save();
+// }, 1000)
