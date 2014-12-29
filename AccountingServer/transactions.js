9dfbf5ae347e6f1628bea249e2bcb500a92c4e3d
John Tan <tantai.john@gmail.com>
2014-12-28 17:24:35 -0800

    Added Transaction.commit

---
+++ AccountingServer/transactions.js
@@ -0,0 +1,25 @@
+var cassandra = require('cassandra-driver');
+
+var Transactions = function(config){
+  this.config = config;
+  this.client = new cassandra.Client({contactPoints: [this.config.cassIp], keyspace:this.config.cassKeyspace});
+};
+
+Transactions.prototype.commit = function(transactions) {
+  var queryOptions = {consistency: cassandra.types.consistencies.quorum};
+  var query = 'INSERT INTO transactions (price, consumerId, energy, blockStart, blockDuration) VALUES (?, ?, ?, ?, ?)';
+  var queries = [];
+  transactions.forEach(function(transaction){
+    queries.push({
+      query: query,
+      params: [transaction.price, transaction.consumerId, transaction.energy, transaction.block.blockStart, transaction.block.blockDuration]
+    })
+  });
+  this.client.batch(queries, queryOptions, function(err){
+    if(err){
+      console.log('err committing', err);
+    }
+  })
+};
+
+module.exports = Transactions;
