8e9fbcde03590799f1b94164a5509f3029e9cf7e
John Tan <tantai.john@gmail.com>
2014-12-29 18:53:38 -0800

    Initial implementation

--- AlternativeBrokerServer/broker.js
+++ AlternativeBrokerServer/broker.js
@@ -27,15 +27,21 @@ Broker.prototype.addSupply = function(supply){
   this.supply[demand.producerId] = supply;
 };
 
-Broker.prototype.collectDemandSupply = function(blockDuration) {
+Broker.prototype.collectDemandSupply = function(timeBlock) {
   this.state = 1;
+  this.timeBlock = timeBlock;
   this.marketNsp.emit('startCollection');
   setTimeout(function(){
     this.systemClient.emit('queryPrice', this.demand);
-  }.bind(this), this.settlementTimePercentage * blockDuration);
+  }.bind(this), this.settlementTimePercentage * timeBlock.blockDuration);
 };
 
 Broker.prototype.settleDemand = function(quote){
+  this.state = 2;
+  // Figure out transactions here
+
+  this.demand = {};
+  this.supply = {};
 
 };
 
