354d410090a1e527249b9107ff3534550b29ff79
John Tan <tantai.john@gmail.com>
2014-12-31 15:03:55 -0800

    Created AddressStore class

--- DiscoveryServer/addressStore.js
+++ DiscoveryServer/addressStore.js
@@ -1,1 +1,19 @@
+var mongoose = require('mongoose');
+
+var AddressStore = function(config){
+  this.state = 0;
+  this.dbClient = mongoose;
+  this.dbClient.connect(config.mongoIp);
+  this.dbClient.connection.on('open', function(){
+    this.state = 1;
+  }.bind(this));
+  this.dbClient.connection.on('error', function(){
+    this.state = 2;
+  }.bind(this));
+};
+
+AddressStore.prototype.getState = function() {
+  return this.state;
+};
+
 module.exports = AddressStore;
