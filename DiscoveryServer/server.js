e642287152ec60cce368f63fc09a979b09452d55
John Tan <tantai.john@gmail.com>
2014-12-31 15:31:32 -0800

    Reformatted files

--- DiscoveryServer/server.js
+++ DiscoveryServer/server.js
@@ -1,24 +1,18 @@
 process.env.node_env = process.env.node_env || "development";
 
 var config = require('./config')[process.env.node_env];
-var fileLog = require('../utils/fileLog');
-global.fileLog = fileLog;
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 // Setup reporter
 var reporter = new (require('../utils/adminReporter'))();
 global.reporter = reporter;
 // Setup middleware
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.json());
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var market = new (require('./market/market'))(config);
-var monitor = new (require('./monitor/monitor'))(config);
-var consumerManager = new (require('./consumerManager'))(config.consumer, market, monitor);
-var producerManager = new (require('./producerManager'))(config.producer, market, monitor);
-// Setup server.
 server.listen(config.port);
 
 // Serve admin
@@ -26,66 +20,27 @@ app.get('/admin', function(req, res){
   res.sendFile(__dirname + '/public/admin.html')
 });
 
-// Serve stats
-app.get('/api/stats', function(req, res){
-  res.json(reporter.update())
-});
-
-console.log("Running the server file");
-console.log("node_env", process.env.node_env); //to check whether it's been set to production when deployed
-
-// Setup listeners for connections on namespaces
-var consumerNsp = io.of('/consumers');
-consumerNsp.on('connection', function(socket){
-  consumerManager.addConsumer(socket);
-});
-
-var producerNsp = io.of('/producers');
-producerNsp.on('connection', function(socket){
-  producerManager.addProducer(socket);
-});
-
-var brokerNsp = io.of('/brokers');
-brokerNsp.on('connection', function(socket){
-  console.log('broker connected');
-  market.on('marketClose', function(auction){
-    socket.emit('marketClose', auction.currentBlock);
-  });
-  socket.on('queryPrice', function(demand){
-    try{
-      var result = market.computeBasedOnDemand(demand.demands);
-      result.timeBlock = demand.timeBlock;
-      result.minPrice = config.minPrice;
-      socket.emit('priceQuote', result);
-    }catch(e){
-      result = {
-        price: config.maxPrice,
-        timeBlock: demand.timeBlock
-      }
-      socket.emit('priceQuote', result);
+// APIs for discovery of nodes.
+app.get('/discover/:role/:subrole', function(req, res){
+  var role = req.params.role;
+  var subrole = req.params.subrole;
+  addressStore.discover(role, subrole, function(err, data){
+    if(err){
+      res.sendStatus(404);
+    }else{
+      res.json(data);
     }
-  });
+  })
 });
 
-/*
-Setup market listeners
-timeBlock is of the form
-{
-  blockStart: 123123124,
-  biddingDuration: 10
-  blockDuration: 120,
-  minPrice: .2,
-  maxPrice: 20
-}
-*/
-
-market.on('startBidding', function(timeBlock){
-  consumerNsp.emit('startBidding', timeBlock);
-  producerNsp.emit('requestSupply', timeBlock);
-});
-
-market.on('changeProduction', function(controls){
-
-});
-
-market.startMarket();
+// APIs for registering node.
+app.post('/register', function(req, res){
+  var opt = req.body;
+  addressStore.register(opt, function(err){
+    if(err){
+      res.sendStatus(500);
+    }else{
+      res.sendStatus(200);
+    }
+  })
+})
