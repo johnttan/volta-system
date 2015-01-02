5c1d1130f4df5781f0e0b65a1b827ec240c0a759
John Tan <tantai.john@gmail.com>
2015-01-01 19:31:57 -0800

    Added spinUp scripts

---
+++ spinupConfig.js
@@ -0,0 +1,28 @@
+module.exports = {
+  nodes: [
+    {
+      name: 'discovery',
+      dir: '/home/john/code/volta-system/DiscoveryServer'
+    },
+    {
+      name: 'accounting',
+      dir: '/home/john/code/volta-system/AccountingServer'
+    },
+    {
+      name: 'consumer',
+      dir: '/home/john/code/volta-consumer'
+    },
+    {
+      name: 'producer',
+      dir: '/home/john/code/volta-producer'
+    },
+    {
+      name: 'broker',
+      dir: '/home/john/code/volta-system/AlternativeBrokerServer'
+    },
+    {
+      name: 'system',
+      dir: '/home/john/code/volta-system/SystemServer'
+    }
+  ]
+}
