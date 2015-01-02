6fd7265b9b5f471a2fa87e79651d2d8144f5acfb
John Tan <tantai.john@gmail.com>
2015-01-01 19:44:11 -0800

    Implemented script

--- spinupConfig.js
+++ spinupConfig.js
@@ -2,27 +2,38 @@ module.exports = {
   nodes: [
     {
       name: 'discovery',
-      dir: '/home/john/code/volta-system/DiscoveryServer'
+      dir: '/home/john/code/volta-system/DiscoveryServer',
+      args: ['server.js']
     },
     {
       name: 'accounting',
-      dir: '/home/john/code/volta-system/AccountingServer'
+      dir: '/home/john/code/volta-system/AccountingServer',
+      args: ['server.js']
     },
     {
       name: 'consumer',
-      dir: '/home/john/code/volta-consumer'
+      dir: '/home/john/code/volta-consumer',
+      args: ['server.js']
+    },
+    {
+      name: 'testConsumers',
+      dir: '/home/john/code/volta-system/AlternativeBrokerServer',
+      args: ['testConsumers.js']
     },
     {
       name: 'producer',
-      dir: '/home/john/code/volta-producer'
+      dir: '/home/john/code/volta-producer',
+      args: ['server.js', 'development']
     },
     {
       name: 'broker',
-      dir: '/home/john/code/volta-system/AlternativeBrokerServer'
+      dir: '/home/john/code/volta-system/AlternativeBrokerServer',
+      args: ['server.js']
     },
     {
       name: 'system',
-      dir: '/home/john/code/volta-system/SystemServer'
+      dir: '/home/john/code/volta-system/SystemServer',
+      args: ['server.js']
     }
   ]
 }
--- spinupNodes.js
+++ spinupNodes.js
@@ -1,2 +1,15 @@
 var spawn = require('child_process').spawn;
-var config = require('spinupConfig');
+var config = require('./spinupConfig');
+
+var procs = {};
+
+config.nodes.forEach(function(node){
+  node.args = node.args || [];
+  node.args[0] = node.dir + '/' + node.args[0];
+  procs[node.name] = spawn('node', node.args)
+  procs[node.name].stdout.on('data', function(data){
+    console.log('from :', node.name, '\n', data.toString())
+  })
+});
+
+
