a54bf7c7f52eca85d77c7501fab7d403c974ae2e
John Tan <tantai.john@gmail.com>
2015-01-01 19:52:37 -0800

    Changed config to include rootDir

--- spinupConfig.js
+++ spinupConfig.js
@@ -1,38 +1,39 @@
 module.exports = {
+  rootDir: '/home/john/code',
   nodes: [
     {
       name: 'discovery',
-      dir: '/home/john/code/volta-system/DiscoveryServer',
+      dir: '/volta-system/DiscoveryServer',
       args: ['server.js']
     },
     {
       name: 'accounting',
-      dir: '/home/john/code/volta-system/AccountingServer',
+      dir: '/volta-system/AccountingServer',
       args: ['server.js']
     },
     {
       name: 'consumer',
-      dir: '/home/john/code/volta-consumer',
+      dir: '/volta-consumer',
       args: ['server.js']
     },
     {
       name: 'testConsumers',
-      dir: '/home/john/code/volta-system/AlternativeBrokerServer',
+      dir: '/volta-system/AlternativeBrokerServer',
       args: ['testConsumers.js']
     },
     {
       name: 'producer',
-      dir: '/home/john/code/volta-producer',
+      dir: '/volta-producer',
       args: ['server.js', 'development']
     },
     {
       name: 'broker',
-      dir: '/home/john/code/volta-system/AlternativeBrokerServer',
+      dir: '/volta-system/AlternativeBrokerServer',
       args: ['server.js']
     },
     {
       name: 'system',
-      dir: '/home/john/code/volta-system/SystemServer',
+      dir: '/volta-system/SystemServer',
       args: ['server.js']
     }
   ]
