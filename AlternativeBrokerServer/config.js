94a9697f92a5fead712ff6bbd2c07ae8f4bb2d21
John Tan <tantai.john@gmail.com>
2015-01-05 12:23:29 -0800

    Updated config

--- AlternativeBrokerServer/config.js
+++ AlternativeBrokerServer/config.js
@@ -4,8 +4,8 @@ exports.development = {
   systemIp: 'http://localhost:8000/brokers',
   brokerFeePercent: .1,
   discountPercent: .5,
-  ip: 'http://104.40.181.157:8001',
-  discoveryIp: 'http://localhost:9000',
+  ip: 'http://localhost:8011',
+  discoveryIp: 'http://104.40.181.157:8001',
   id: 25,
   role: 'system',
   subRole: 'broker'
