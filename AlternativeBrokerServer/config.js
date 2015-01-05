64d4656a95b80d02af4b5c8f42c2a4e5b0e45083
John Tan <tantai.john@gmail.com>
2015-01-05 10:24:27 -0800

    Integrated Broker

--- AlternativeBrokerServer/config.js
+++ AlternativeBrokerServer/config.js
@@ -3,7 +3,12 @@ exports.development = {
   settlementTimePercentage: .50,
   systemIp: 'http://localhost:8000/brokers',
   brokerFeePercent: .1,
-  discountPercent: .5
+  discountPercent: .5,
+  ip: 'http://localhost:8011',
+  discoveryIp: 'http://localhost:9000',
+  id: 25,
+  role: 'system',
+  subRole: 'broker'
 };
 
 exports.production = {
