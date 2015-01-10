0a4d790c9f37c6d6efe16baa21354a2a72cbf468
John Tan <tantai.john@gmail.com>
2015-01-09 23:19:40 -0800

    Added transactionsService

---
+++ SystemServer/public/app/services/TransactionsService.js
@@ -0,0 +1,33 @@
+angular.module('app')
+  .factory('TransactionsService', function(){
+    var transactionsSocket = io('130.211.159.31/subscriptions');
+    transactionsSocket.emit('subscribe', {
+      key: 'buyer',
+      subkey: 'AEB'
+    });
+    transactionsSocket.emit('subscribe', {
+      key: 'seller',
+      subkey: 'AEB'
+    });
+    transactionsSocket.emit('subscribe', {
+      key: 'buyer',
+      subkey: 'grid'
+    });
+    transactionsSocket.emit('subscribe', {
+      key: 'seller',
+      subkey: 'grid'
+    });
+    var transactions = {};
+    var result = {
+      transactions: transactions,
+      update: function(){},
+      addUpdate: function(update){
+        result.update = update;
+      }
+    };
+    aggSocket.on('transaction', function(data){
+      transactions.eq(data);
+      result.update();
+    });
+    return result;
+  })
