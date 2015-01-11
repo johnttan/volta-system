091eeb3d947efb8aac50aa671a12549966a29f55
John Tan <tantai.john@gmail.com>
2015-01-11 12:39:49 -0800

    Implemented consumer demand directive

--- SystemServer/public/app/dashboard/dashboardBoxes.directives.js
+++ SystemServer/public/app/dashboard/dashboardBoxes.directives.js
@@ -10,7 +10,8 @@ angular.module('app')
         $scope.tile = {
           color: 'purple',
           icon: 'icon-arrow-up',
-          title: 'Price'
+          title: 'Price',
+          class: 'price'
         };
 
         $scope.data = function(){
@@ -34,7 +35,7 @@ angular.module('app')
               }
             });
             result[0] = 0;
-            $('.boxchart.' + $scope.tile.title).sparkline(result, {
+            $('.boxchart.' + $scope.tile.class).sparkline(result, {
               type: 'bar',
               height: '60',
               barWidth: '4',
@@ -63,15 +64,54 @@ angular.module('app')
         stats: "="
       },
       controller: function($scope){
+        var data = new CircularBuffer(14);
         $scope.tile = {
           color: 'green',
-          data: [5,6,7,2,0,-4,-2,4,8,2,3,3,2],
           number: '123,000',
           icon: 'icon-arrow-up',
-          title: 'Consumer Demand'
+          title: 'Consumer Demand',
+          class: 'consumerDemand'
         };
+
         $scope.data = function(){
-          return [5,6,7,2,0,-4,-2,4,8,2,3,3,2].join(',');
+          if($scope.stats){
+            var auctionsList = $scope.stats.auctions.array;
+            var max = 0;
+            for(var i = 1; i < 15; i++){
+              var auction = auctionsList[auctionsList.length - i];
+              if(auction && auction.bids[0]){
+                var sum = 0;
+                auction.bids.forEach(function(bid){
+                  sum += bid.energy;
+                });
+                if(sum > max){
+                  max = sum;
+                };
+                data.eq(sum);
+              }
+            };
+            var result = [];
+            data.array.forEach(function(el){
+              if(el){
+                result.push(Math.floor((el / max) * 10));
+              }
+            });
+            result[0] = 0;
+            $('.boxchart.' + $scope.tile.class).sparkline(result, {
+              type: 'bar',
+              height: '60',
+              barWidth: '4',
+              barSpacing: '1',
+              barColor: '#ffffff',
+              negBarColor: '#eeeeee'
+            });
+          };
+        };
+
+        $scope.number = function(){
+          if(data.array && data.array.length >= 1){
+            return Math.floor(data.array[data.array.length-1]).toString() + ' MW';
+          }
         };
       },
       templateUrl: 'statBox.html',
