8cebf4efe3ad642c360960a7ddf97d19338a6c4b
John Tan <tantai.john@gmail.com>
2015-01-11 12:10:50 -0800

    Updated sparkCharts

--- SystemServer/public/app/dashboard/dashboardBoxes.directives.js
+++ SystemServer/public/app/dashboard/dashboardBoxes.directives.js
@@ -6,7 +6,7 @@ angular.module('app')
         stats: "="
       },
       controller: function($scope){
-        var data = new CircularBuffer(13);
+        var data = new CircularBuffer(14);
         $scope.tile = {
           color: 'purple',
           icon: 'icon-arrow-up',
@@ -16,13 +16,32 @@ angular.module('app')
         $scope.data = function(){
           if($scope.stats){
             var auctionsList = $scope.stats.auctions.array;
-            for(var i = 1; i < 14; i++){
+            var max = 0;
+            for(var i = 1; i < 15; i++){
               var auction = auctionsList[auctionsList.length - i];
               if(auction && auction.receipts.receipts[0]){
-                data.eq(auction.receipts.receipts[0].price)
+                var price = auction.receipts.receipts[0].price;
+                if(price > max){
+                  max = price;
+                };
+                data.eq(price);
               }
             };
-            return data.array.join(',');
+            var result = [];
+            data.array.forEach(function(el){
+              if(el){
+                result.push((el / max) * 10);
+              }
+            });
+            result[0] = 0;
+            $('.boxchart.' + $scope.tile.title).sparkline(result, {
+              type: 'bar',
+              height: '60',
+              barWidth: '4',
+              barSpacing: '1',
+              barColor: '#ffffff',
+              negBarColor: '#eeeeee'
+            });
           }
         };
 
@@ -50,7 +69,10 @@ angular.module('app')
           number: '123,000',
           icon: 'icon-arrow-up',
           title: 'Consumer Demand'
-        }
+        };
+        $scope.data = function(){
+          return [5,6,7,2,0,-4,-2,4,8,2,3,3,2].join(',');
+        };
       },
       templateUrl: 'statBox.html',
       replace: true
@@ -69,7 +91,11 @@ angular.module('app')
           number: '$900,900',
           icon: 'icon-arrow-up',
           title: 'Grid Sales'
-        }
+        };
+
+        $scope.data = function(){
+          return [5,6,7,2,0,-4,-2,4,8,2,3,3,2].join(',');
+        };
       },
       templateUrl: 'statBox.html',
       replace: true
@@ -88,7 +114,11 @@ angular.module('app')
           number: '$100,099',
           icon: 'icon-arrow-down',
           title: 'Alternative Sales'
-        }
+        };
+
+        $scope.data = function(){
+          return [5,6,7,2,0,-4,-2,4,8,2,3,3,2].join(',');
+        };
       },
       templateUrl: 'statBox.html',
       replace: true
