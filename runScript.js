f75f303d014feb8d02cdbef1a59f32bb21f2433a
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-18 14:27:34 -0800

    Prepared broker for deployment

--- runScript.js
+++ runScript.js
@@ -4,6 +4,7 @@ if (process.env.AGENT === "system") {
   console.log("choosing system run script");
   spawn('node', ['SystemServer/server.js']);	
 } else if (process.env.AGENT === "broker") {
+  console.log("choosing broker run script");
   spawn('node', ['AlternativeBrokerServer/server.js'])
 } else {
   console.log("AGENT variable should be set to system or broker")
