ff9acc6fd20f890e85c36ea7e4de0869d7cded82
John Tan <tantai.john@gmail.com>
2015-01-18 15:30:21 -0800

    Added AzureClient

--- utils/azureDeploy.js
+++ utils/azureDeploy.js
@@ -1,9 +1,9 @@
 var exec = require('child_process').exec;
 var _ = require('lodash');
 
-var AzureClient = function(){
+var AzureClient = function(readyCB){
   this.sites = {};
-  this.list();
+  this.list(readyCB);
 };
 
 AzureClient.prototype.list = function(cb) {
@@ -14,16 +14,79 @@ AzureClient.prototype.list = function(cb) {
     }else{
       out = JSON.parse(out);
       out.forEach(function(el){
-        var newSite = {};
+        var newSite = _.pick(el, ['name', 'hostNames', 'repositorySiteName', 'state', 'siteProperties']);
         this.sites[el.name] = newSite;
-      }.bind(this))
+      }.bind(this));
+      cb(null, this.sites)
     };
-    console.log(this.sites);
   }.bind(this))
 };
 
-AzureClient.prototype.stopAll = function(name) {
-
+AzureClient.prototype.start = function(name, cb){
+  if(this.sites[name]){
+    exec('azure site start ' + name, function(err, out){
+      if(err){
+        cb(false)
+      }else{
+        cb(true)
+      }
+    })
+  }else{
+    cb(false)
+  }
 };
 
-var test = new AzureClient();
+AzureClient.prototype.stop = function(name, cb){
+  if(this.sites[name]){
+    exec('azure site stop ' + name, function(err, out){
+      if(err){
+        cb(false)
+      }else{
+        cb(true)
+      }
+    })
+  }else{
+    cb(false)
+  }
+};
+
+AzureClient.prototype.stopAll = function(nsp, cb) {
+  var numStarted = 0;
+  var numStopped = 0;
+  for(var key in this.sites){
+    if(key.indexOf(nsp) > -1){
+      numStarted ++;
+      this.stop(key, function(){
+        numStopped ++;
+        if(numStopped === numStarted){
+          cb(true)
+          this.list()
+        }
+      }.bind(this))
+    }
+  }
+};
+
+AzureClient.prototype.startAll = function(nsp, cb) {
+  var numStarted = 0;
+  var numStopped = 0;
+  for(var key in this.sites){
+    if(key.indexOf(nsp) > -1){
+      numStarted ++;
+      this.start(key, function(){
+        numStopped ++;
+        if(numStopped === numStarted){
+          cb(true)
+          this.list()
+        }
+      }.bind(this))
+    }
+  }
+};
+
+var test = new AzureClient(function(){
+  test.stopAll('volta', function(){
+    console.log('stoppedAll');
+  })
+});
+
