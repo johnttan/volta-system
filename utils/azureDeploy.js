a28f5fc023a0d51fab6929281a1324c6943dd494
John Tan <tantai.john@gmail.com>
2015-01-18 16:24:22 -0800

    Added create methods

--- utils/azureDeploy.js
+++ utils/azureDeploy.js
@@ -22,12 +22,42 @@ AzureClient.prototype.list = function(cb) {
   }.bind(this))
 };
 
+AzureClient.prototype.create = function(name, nsp, appsettings, cb){
+  this._siteCommand('create', [name, '--location "West US"'], function(err, out){
+    if(err){
+      cb(err)
+    }else{
+      var started = 0;
+      var stopped = 0;
+      for(var setting in appsettings){
+        started ++;
+        this.setSetting(name, setting, appsettings[setting], function(err, out){
+          stopped ++;
+          if(stopped === started){
+            this.restart(name, function(err){
+              cb(err)
+            });
+          }
+        }.bind(this));
+      }
+    }
+  }.bind(this))
+};
+
+AzureClient.prototype.setSetting = function(name, key, value, cb) {
+  this._siteCommand('appsetting', ['add', key+'='+value, name], cb);
+};
+
 AzureClient.prototype.start = function(name, cb){
   this._siteCommand('start', name, cb);
 };
 
 AzureClient.prototype.stop = function(name, cb){
-  this._siteCommand('stop', name, cb);
+  this._siteCommand('stop', [name, '-q'], cb);
+};
+
+AzureClient.prototype.delete = function(name, cb){
+  this._siteCommand('delete', [name, '-q'], cb);
 };
 
 AzureClient.prototype.restart = function(name, cb) {
@@ -46,11 +76,19 @@ AzureClient.prototype.restartAll = function(nsp, cb){
   this._siteCommandAll('restart', nsp, cb);
 };
 
+AzureClient.prototype.deleteAll = function(nsp, cb){
+  this._siteCommandAll('delete', nsp, cb);
+};
+
 AzureClient.prototype._siteCommandAll = function(command, arg, cb){
   var numStarted = 0;
   var numStopped = 0;
+  var name = arg;
+  if(Array.isArray(arg)){
+    name = arg[0];
+  };
   for(var key in this.sites){
-    if(key.indexOf(arg) > -1){
+    if(key.indexOf(name) > -1){
       numStarted ++;
       this[command](key, function(err, out){
         numStopped ++;
@@ -64,6 +102,9 @@ AzureClient.prototype._siteCommandAll = function(command, arg, cb){
 };
 
 AzureClient.prototype._siteCommand = function(command, arg, cb){
+  if(Array.isArray(arg)){
+    arg = arg.join(' ');
+  };
   exec('azure site ' + command + ' ' + arg, function(err, out){
     if(err){
       cb(err)
@@ -75,8 +116,20 @@ AzureClient.prototype._siteCommand = function(command, arg, cb){
 
 var test = new AzureClient(function(err){
   if(err){console.log(err)};
-  test.startAll('volta', function(){
-    console.log('startAll');
-  });
+  // test.deleteAll('volta', function(err, out){
+  //   console.log(err, out);
+  // })
+  // test.setSetting('volta-p8', 'SITE_TYPE', 'backend', function(err){
+  //   if(err){console.log(err)};
+  //   console.log('setting set')
+  // });
+  test.create('volta-p9', 'volta', {
+    SITE_TYPE: 'backend'
+  }, function(err){
+    if(err){
+      console.log(err)
+    }
+    console.log('site created')
+  })
 });
 
