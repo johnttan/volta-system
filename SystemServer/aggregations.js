43a21e4003c64ca110eab156cc5c77b6f49b55e2
John Tan <tantai.john@gmail.com>
2015-01-19 11:36:36 -0800

    Fixed bug with aggregations reporting producers

--- SystemServer/aggregations.js
+++ SystemServer/aggregations.js
@@ -38,6 +38,11 @@ var aggregations = [
     key: 'producers.disconnect',
     aggregator: function(newValue, oldStructure){
       oldStructure.num --;
+      if(oldStructure.supply[newValue]){
+        delete oldStructure.supply[newValue];
+        delete oldStructure.ids[newValue];
+        delete oldStructure.controls[newValue];
+      };
       return oldStructure;
     }
   },
