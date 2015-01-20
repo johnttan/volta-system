56cb2cb95f0179952b5301e3d3d9650e78166f84
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-19 23:48:59 -0800

    Empty commit

--- AlternativeBrokerServer/server.js
+++ AlternativeBrokerServer/server.js
@@ -13,6 +13,7 @@ var bodyParser = require('body-parser');
 var reporter = new (require('../utils/adminReporter'))();
 global.reporter = reporter;
 console.log("running broker server file");
+
 // Setup middleware
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
